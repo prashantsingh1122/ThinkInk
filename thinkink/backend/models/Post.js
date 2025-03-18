4f1b20d1b2ba36bbec38e0c488f2db463aa306b4
prashantsingh1122 <prashantsingh3517@gmail.com>
2025-03-18 23:11:13 +0530

    Starting of CRUD operation

--- thinkink/backend/models/Post.js
+++ thinkink/backend/models/Post.js
@@ -2,10 +2,24 @@ import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    image: { type: String },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    image: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
