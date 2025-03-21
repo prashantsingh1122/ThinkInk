86c939daec19515bb6d093e703059062721d2137
prashantsingh1122 <prashantsingh3517@gmail.com>
2025-03-21 23:34:16 +0530

    y

--- thinkink/frontend/src/services/api.js
+++ thinkink/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/posts"; // Corrected URL
+const API_BASE_URL =  "http://localhost:5000/api/posts"; // Corrected URL
 // ✅ Signup function
 export const signup = async (userData) => {
   try {
