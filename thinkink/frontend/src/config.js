c92371a0a7d28f5b17e8965481b8cb11c5143f7f
prashantsingh1122 <prashantsingh3517@gmail.com>
2025-04-26 15:47:19 +0530

    --

--- thinkink/frontend/src/config.js
+++ thinkink/frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: import.meta.env.VITE_API_URL, // Get the URL from the environment variable
+  apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 };
 
 export default config;
