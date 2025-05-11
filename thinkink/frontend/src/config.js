4b0f4369e7e275a033bccdb5f269a956a48cce4f
prashantsingh1122 <prashantsingh3517@gmail.com>
2025-05-11 22:16:40 +0530

    vercerl hosting

--- thinkink/frontend/src/config.js
+++ thinkink/frontend/src/config.js
@@ -1,6 +1,7 @@
 const config = {
-  apiUrl: import.meta.env.VITE_API_BASE_URL || "/api",
+  apiUrl: import.meta.env.VITE_API_BASE_URL || 'https://thinkink-backend.onrender.com/api',
 };
 
 export default config;
 
+//change it to /api for local host and in env also
